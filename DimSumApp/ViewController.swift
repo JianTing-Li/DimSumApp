4093f7e44eb5aa2996f7a2e81df9489167809a46
Jian Ting Li <jiantingli@Jian-Tings-MacBook-Pro.local>
2019-07-11 11:48:50 -0400

    first commit

---
+++ DimSumApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DimSumApp
+//
+//  Created by Jian Ting Li on 7/11/19.
+//  Copyright © 2019 Jian Ting Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("OK")
+    }
+
+
+}
+
