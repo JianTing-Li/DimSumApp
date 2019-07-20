e0c35387dab1ac161445d93c0c93b705841ce067
Jabeen Cheema <jabeencheema@pursuit.org>
2019-07-20 17:04:25 -0400

    rearranged files and added files, also worked on dimsum UI

--- DimSumApp/Controller/DimSumController.swift
+++ DimSumApp/Controller/DimSumController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class DimSumController: UIViewController {
 
+    @IBOutlet weak var foodCollectionView: UICollectionView!
+    let testData = ["Dim Sum 1", "Dim Sum 2", "Dim Sum 3"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let nib = UINib.init(nibName: "FoodCell", bundle: nil)
+        foodCollectionView.register(nib, forCellWithReuseIdentifier: "FoodCell")
+        foodCollectionView.dataSource = self
+        foodCollectionView.delegate = self
     }
 
+}
+
+extension DimSumController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 800/2, height: 500/2)
+    }
+}
+
+extension DimSumController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let foodCell = collectionView.dequeueReusableCell(withReuseIdentifier: "FoodCell", for: indexPath) as? FoodCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+//        foodCell.chineseFoodLabel.text = testData[indexPath.row]
+    return foodCell
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
 }
---
+++ DimSumApp/View/DimSum/FoodCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FoodCollectionViewCell.swift
+//  DimSumApp
+//
+//  Created by Jabeen's MacBook on 7/17/19.
+//  Copyright © 2019 Jian Ting Li. All rights reserved.
+//
+
+import UIKit
+
+class FoodCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var foodImageView: UIImageView!
+    @IBOutlet weak var engFoodLabel: UILabel!
+    @IBOutlet weak var chineseFoodLabel: UILabel!
+    
+
+
+
+
+
+
+}
