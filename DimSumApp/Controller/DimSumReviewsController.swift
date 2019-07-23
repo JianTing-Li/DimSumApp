013d5cc1d8d0a83f3f09fb61d0f51553278d3d6f
Jian Ting Li <jiantingli@Jian-Tings-MacBook-Pro.local>
2019-07-23 01:45:49 -0400

    dim sum images added and connect storyboard elements to VCs

--- DimSumApp/Controller/DimSumReviewsController.swift
+++ DimSumApp/Controller/DimSumReviewsController.swift
@@ -15,5 +15,11 @@ class DimSumReviewsController: UITableViewController {
 
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
