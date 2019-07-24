826b3cb8d5e628b1a08a0bdb1d0c69e71a0d0a6f
Jian Ting Li <jiantingli@Jian-Tings-MacBook-Pro.local>
2019-07-24 03:31:38 -0400

    partially setup write a review screen

--- DimSumApp/Controller/DimSum/DimSumWriteReviewController.swift
+++ DimSumApp/Controller/DimSum/DimSumWriteReviewController.swift
@@ -7,13 +7,56 @@
 //
 
 import UIKit
+import Cosmos
 
 class DimSumWriteReviewController: UIViewController {
 
+    @IBOutlet weak var rating: CosmosView!
+    @IBOutlet weak var reviewTextView: UITextView!
+    
+    private var capturedRating: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // setup cosmos
+//        // Called when user finishes changing the rating by lifting the finger from the view.
+//        // This may be a good place to save the rating in the database or send to the server.
+//        cosmosView.didFinishTouchingCosmos = { rating in }
+        
+        
+        // setup text field (refer to fellow blogger)
     }
     
-
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func postButtonPressed(_ sender: UIBarButtonItem) {
+        // check if user give a rating and description (make sure it's not empty)
+        // instatiate review and save to data persistence
+    }
+    
 }
+
+
+//private func configureTextView() {
+//    configureInputAccessoryView()
+//    blogDescriptionTextView.delegate = self
+//    blogDescriptionTextView.textColor = .lightGray
+//    blogDescriptionTextView.text = Constants.BlogDescriptionPlaceholder
+//}
+//
+//extension AddBlogController: UITextViewDelegate {
+//    func textViewDidBeginEditing(_ textView: UITextView) {
+//        if textView.text == Constants.BlogDescriptionPlaceholder {
+//            textView.textColor = .black
+//            textView.text = ""
+//        }
+//    }
+//    func textViewDidEndEditing(_ textView: UITextView) {
+//        if textView.text == "" {
+//            textView.textColor = .lightGray
+//            textView.text = Constants.BlogDescriptionPlaceholder
+//        }
+//    }
+//}
